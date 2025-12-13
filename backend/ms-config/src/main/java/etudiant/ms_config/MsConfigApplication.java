d96dcdb4df95b2563d37d1cf911fcddf8d8497c3
Safy <s.d81@zig.univ.sn>
2025-12-13 21:21:14 +0000

    configuration de ms-config

--- backend/ms-config/src/main/java/etudiant/ms_config/MsConfigApplication.java
+++ backend/ms-config/src/main/java/etudiant/ms_config/MsConfigApplication.java
@@ -1,5 +1,4 @@
 package uasz.etudiant.ms_config;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
